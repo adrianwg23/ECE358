868fa352fc39e0003c590b4729b920b59c80424e
adrianwg23 <adrianwg23@hotmail.com>
2021-11-14 23:22:26 -0500

    idk what i did

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -123,18 +123,17 @@ class PersisentCSMACD:
                         break
         else:
             sender_node.queue.popleft()
-            for i in range(self.N):
-                # Check if each node is busy, if so change their events to total transmission time
-                curr_node = self.nodes[i]
-                total_transmission_time = sender_frame_time + compute_propagation_delay(self.D, self.S)*abs(i-sender_index) + compute_transmission_delay(self.L, self.R)
-                for i in range(len(curr_node.queue)):
-                    frame_time = curr_node.queue[i]
-                    # bubble the new receiver_frame_time to all events that have frame_time less than this time
-                    if frame_time < total_transmission_time:
-                        curr_node.queue[i] = total_transmission_time
-                    else:
-                        break
+            total_transmission_time = sender_frame_time + compute_transmission_delay(self.L, self.R)
+            for i in range(len(sender_node.queue)):
+                frame_time = sender_node.queue[i]
+                # bubble the new transmission to all events that have frame_time less than this time
+                if frame_time < total_transmission_time:
+                    sender_node.queue[i] = total_transmission_time
+                else:
+                    break
+                    
             self.successful_packet_transmission += 1
+            
         if not sender_node.queue:
             self.completed_nodes += 1
 
@@ -173,11 +172,11 @@ class PersisentCSMACD:
                         else:
                             break
                     
-                    collision_status["collision_detected"] = True
-                    collision_status["max_distance"] = max(max_distance, distance_to_sender)
+                collision_status["collision_detected"] = True
+                collision_status["max_distance"] = max(max_distance, distance_to_sender)
 
-            # collision on the medium, but current node has no collision and senses medium is busy
-            if collision_status["collision_detected"] and head_frame_time > received_time and head_frame_time < received_time + compute_transmission_delay(self.L, self.R):
+            # check if medium is busy
+            if head_frame_time > received_time and head_frame_time < received_time + compute_transmission_delay(self.L, self.R):
                 transmission_time = received_time + compute_transmission_delay(self.L, self.R)
                 for i in range(len(curr_node.queue)):
                     frame_time = curr_node.queue[i]
